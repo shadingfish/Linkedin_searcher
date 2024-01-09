74033438960bd88edf2a97d206ca84167526b1f9
yudingyi <yudingyi054@gmail.com>
2024-01-09 01:01:04 -0800

    parser

--- ice_breaker.py
+++ ice_breaker.py
@@ -6,6 +6,7 @@ from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 # from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 # from third_parties.twitter import scrape_user_tweets
+from . import output_parsers
 
 
 def ice_break(name: str) -> str:
@@ -14,21 +15,23 @@ def ice_break(name: str) -> str:
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
     summary_template = """
-         given the Linkedin information {linkedin_information} and twitter {twitter_information} about a person from I want you to create:
+         given the Linkedin information {linkedin_information} about a person from I want you to create:
          1. a short summary
          2. two interesting facts about them
          3. A topic that may interest them
-         4. 2 creative Ice breakers to open a conversation with them 
+         4. 2 creative Ice breakers to open a conversation with them
+         \n{format_instruction}
      """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_information"],
         template=summary_template,
+        partial_variables = {"format_instruction": output_parsers.person_intel_parser.get_format_instruction()}
     )
 
     llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    result = chain.run(information=linkedin_data)
+    result = chain.run(linkedin_information=linkedin_data)
     print(result)
     return result
     # tweets = scrape_user_tweets(username="@elonmusk", num_tweets=100)
@@ -37,4 +40,4 @@ def ice_break(name: str) -> str:
 
 if __name__ == "__main__":
     print("Hello LangChain!")
-    ice_break(name="Harrison Chase")
+    result = ice_break(name="Harrison Chase")
