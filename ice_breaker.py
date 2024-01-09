a74e9adbce59e0fd7bdafe5e92f6fe7b8a145692
yudingyi <yudingyi054@gmail.com>
2024-01-09 01:08:50 -0800

    add a parser

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,14 +3,15 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+
 # from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
+
 # from third_parties.twitter import scrape_user_tweets
-from . import output_parsers
+from output_parsers import person_intel_parser, PersonIntel
 
 
-def ice_break(name: str) -> str:
-    
+def ice_break(name: str) -> PersonIntel:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
@@ -26,14 +27,16 @@ def ice_break(name: str) -> str:
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_information"],
         template=summary_template,
-        partial_variables = {"format_instruction": output_parsers.person_intel_parser.get_format_instruction()}
+        partial_variables={
+            "format_instruction": person_intel_parser.get_format_instruction()
+        },
     )
 
     llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    result = chain.run(linkedin_information=linkedin_data)
-    print(result)
-    return result
+    chain_result = chain.run(linkedin_information=linkedin_data)
+    # print(result)
+    return person_intel_parser.parse(chain_result)
     # tweets = scrape_user_tweets(username="@elonmusk", num_tweets=100)
     # print(tweets)
 
--- third_parties/twitter.py
+++ third_parties/twitter.py
@@ -17,7 +17,6 @@ auth.set_access_token(
 api = tweepy.API(auth)
 
 
-
 def scrape_user_tweets(username, num_tweets=20):
     """
     Scrapes a Twitter user's original tweets (i.e., not retweets or replies) and returns them as a list of dictionaries.
